a858a931d9adddae5fc015f57215f722c5547bc7
newstronger <96866360+newstronger@users.noreply.github.com>
2024-06-20 19:15:54 +0800

    Update test_restore.py

--- test_restore.py
+++ test_restore.py
@@ -82,13 +82,16 @@ def test():
                         sub_pred=net.forward(sub_img)
                         pred[:,:,i:i+512,j:j+512]=sub_pred
             if opt.model_name=='mix':
-                ori_pred1=pred3
+                ori_pred1=pred1
+                ori_pred2=pred3
                 pred1=(pred1>opt.threshold[1]).float()
                 pred2=(pred2>opt.threshold[1]).float()
                 pred3=(pred3>opt.threshold[1]).float()
                 pred4=(pred4>opt.threshold[1]).float()
                 pred=((pred1+pred2+pred3+pred4)>1).float()
-                pred=torch.max(ori_pred1,pred)
+                pred_1=(torch.max(ori_pred1,pred)>0.85).float()
+                pred_2=(torch.max(ori_pred2,pred)>0.7).float()
+                pred=torch.max(pred_1,pred_2)
             pred=pred[:,:,:size[0],:size[1]]
             ### save img
             if opt.save_img == True:
